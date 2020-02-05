dee32784ac726c551e725f2fa11ef11920a13662
Zachary Hagman <zach@cuddlyspork.com>
2020-02-05 07:16:26 -0800

    Added TabbedView

--- InDebtly/Views/MainInterfaceView.swift
+++ InDebtly/Views/MainInterfaceView.swift
@@ -14,12 +14,26 @@ struct MainInterfaceView: View {
 		ZStack {
 			Color(Primaries.SkyBlue).edgesIgnoringSafeArea(.all)
 			VStack(alignment: .center) {
+				Spacer()
 				Text("InDebted")
 					.scaledFont(name: Fonts.futuraBoldCondensed, size: 50)
 					.foregroundColor(.white)
+				Spacer()
 				ActivityIndicatorView()
 					.frame(width: 150, height: 150)
-				
+				Spacer()
+				Button(action: {
+					print("Login Button")
+				}) {
+					Text("Unlock")
+						.padding([.top, .bottom], 24)
+						.padding([.leading, .trailing], 48)
+						.foregroundColor(.white)
+						.scaledFont(name: Fonts.futuraMedCondensed, size: 36)
+						.background(Color(Primaries.NavyBlue))
+						.cornerRadius(24)
+
+				}
 			}
 		}
     }
---
+++ InDebtly/Views/TabbedView.swift
@@ -0,0 +1,44 @@
+//
+//  TabbedView.swift
+//  InDebtly
+//
+//  Created by Zachary Hagman on 2/4/20.
+//  Copyright © 2020 Rogue Spork, LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct TabbedView: View {
+	@State var selectedIndex: Int
+
+	var body: some View {
+		TabView(selection: $selectedIndex) {
+
+			OverviewView().tabItem {
+				Image(systemName: selectedIndex == 0 ? "house" : "house.fill")
+				Text("Home")
+			}.tag(1)
+
+			SavingsView().tabItem {
+				Image(systemName: selectedIndex == 1 ? "chevron.up.square" : "chevron.up.square.fill")
+				Text("Savings")
+			}.tag(2)
+
+			DebtView().tabItem {
+				Image(systemName: "chevron.down.square.fill")
+				Text("Debts")
+			}.tag(3)
+
+			SettingsView().tabItem {
+				Image(systemName: "slider.horizontal.3")
+				Text("Settings")
+			}.tag(4)
+		}.accentColor(.pink)
+    }
+}
+
+//struct TabbedView_Previews: PreviewProvider {
+//    static var previews: some View {
+//		TabbedView(selectedIndex: 2)
+//    }
+//}
