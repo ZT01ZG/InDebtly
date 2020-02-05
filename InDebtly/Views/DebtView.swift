95dbf85eed82422f4e59f158f84fd21365c08af2
Zachary Hagman <zach@cuddlyspork.com>
2020-02-05 07:16:50 -0800

    Add Temp/Placeholder views, Constants file

---
+++ InDebtly/Views/DebtView.swift
@@ -0,0 +1,21 @@
+//
+//  DebtView.swift
+//  InDebtly
+//
+//  Created by Zachary Hagman on 2/4/20.
+//  Copyright © 2020 Rogue Spork, LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct DebtView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DebtView_Previews: PreviewProvider {
+    static var previews: some View {
+        DebtView()
+    }
+}
